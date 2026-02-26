3ad33c1234250b84a16f314c78c25422ab44180a
h-kono-it <h.kono.it@gmail.com>
2026-02-26 22:50:50 +0900

    remove deployctl settings

--- main.ts
+++ main.ts
@@ -11,12 +11,6 @@ Deno.cron(
   "0 0 * * 1,4",
   async () => {
     console.log("--- Deno Cron 実行開始 (JST 9:00) ---");
-    if (await isPreview()) {
-      console.log(
-        "プレビューモードのため、フォーラムの投稿通知をスキップします。",
-      );
-      return;
-    }
     await notifyActiveForumPosts();
     console.log("--- Deno Cron 実行終了 ---");
   },
@@ -31,10 +25,6 @@ Deno.cron(
   "0 1 * * *",
   async () => {
     console.log("--- Deno Cron 実行開始 (JST 10:00) ---");
-    if (await isPreview()) {
-      console.log("プレビューモードのため、新しい音楽の通知をスキップします。");
-      return;
-    }
     await notifyNewMusics();
     console.log("--- Deno Cron 実行終了 ---");
   },
@@ -42,17 +32,13 @@ Deno.cron(
 
 /**
  * ランダムで話題を特定チャンネルに投稿する周期の定義
- * 毎週月曜日の10時00分 JST
+ * 毎週月曜日の19時00分 JST
  */
 Deno.cron(
   "Random Topic Notification",
   "0 10 * * 2",
   async () => {
     console.log("--- Deno Cron 実行開始 (話題提供) ---");
-    if (await isPreview()) {
-      console.log("プレビューモードのため、話題の提供をスキップします。");
-      return;
-    }
     await notifyTopic();
     console.log("--- Deno Cron 実行終了 ---");
   },
@@ -60,28 +46,15 @@ Deno.cron(
 
 /**
  * ランダムで話題を英語で特定チャンネルに投稿する周期の定義
- * 毎週木曜日の10時00分 JST
+ * 毎週木曜日の19時00分 JST
  */
 Deno.cron(
   "Random English Topic Notification",
   "0 10 * * 5",
   async () => {
     console.log("--- Deno Cron 実行開始 (英語の話題提供) ---");
-    if (await isPreview()) {
-      console.log(
-        "プレビューモードのため、話題の提供（英語）をスキップします。",
-      );
-      return;
-    }
     await notifyTopic(true);
     console.log("--- Deno Cron 実行終了 ---");
   },
 );
 
-async function isPreview(): Promise<boolean> {
-  const kv = await Deno.openKv();
-  const preview = await kv.get<boolean>(["is_preview"]);
-  console.log("Preview Status:", preview.value);
-  kv.close();
-  return preview.value || false;
-}
